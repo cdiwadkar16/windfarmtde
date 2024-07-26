462f1b8f1b217decbf6df71ce376643b7d779b4a
Chait Diwadkar <94201190+cdiwadkar16@users.noreply.github.com>
2024-07-26 12:25:52 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,16 +3,19 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 import taosws
+import taosrest
 
+from datetime import datetime
 """
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+# Renewable Energy Example
 """
+
+start_time = st.slider(
+    "When do you start?",
+    value=datetime(2020, 1, 1, 9, 30),
+    format="MM/DD/YY - hh:mm",
+)
+st.write("Start time:", start_time)
 
 num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
 num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
